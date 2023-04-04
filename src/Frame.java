f8a3af04805e9cf62d1a49d55d3413ea78fe5ff1
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-04-04 20:45:45 +0545

    basic save func done

--- src/Frame.java
+++ src/Frame.java
@@ -1,17 +1,33 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class Frame extends JFrame implements ActionListener {
 
     JFileChooser fileChooser;
+
+//    FileNameExtensionFilter fileNameExtensionFilter;
+    String text;
+    FileReader fileReader;
+    FileWriter fileWriter;
+
+    File file;
     JMenuBar menu = new JMenuBar();
     JMenu fileMenu = new JMenu("File");
     JMenuItem fileMenuItemSaveAs = new JMenuItem("Save As");
     JTextArea textArea;
 
     Frame(){
+
+//       fileNameExtensionFilter = new FileNameExtensionFilter();
+
         setMenu();
 
         setTextArea();
@@ -44,8 +60,59 @@ public class Frame extends JFrame implements ActionListener {
     {
         if(e.getSource() == fileMenuItemSaveAs)
         {
-            fileChooser = new JFileChooser();
+            try {
+                saveFile();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
+    public File setExtension(File file){
+
+        String name = file.getName();  //get name
+        String parentPath = file.getParent();
+        String ext = "";
+        String url = "";
+        //get the extension
+        if(name.length() > 4) {
+
+            for (int i = name.length() - 4; i < name.length(); i++) {
+                ext += name.charAt(i);
+            }
+        }
+        if(ext != ".txt")
+        {
+            url = parentPath + "/" + name + ".txt";
+        }
+        return new File(url);
+
+    }
+
+    public void saveFile() throws IOException {
+        boolean status;
+
+        fileChooser = new JFileChooser();
+
+        int response = fileChooser.showSaveDialog(null); //show dialog box
+
+        if(response == JFileChooser.APPROVE_OPTION)
+        {
+
+            file = setExtension(fileChooser.getSelectedFile());  //get the selected file
+            System.out.println(file.getParent());
+             status =   file.createNewFile();
+            //writing into the file
+            if(status)
+            {
+                fileWriter = new FileWriter(file);
+                text =  textArea.getText();
+                fileWriter.write(text);
+                fileWriter.close();
+            }
+
 
         }
+
     }
 }
